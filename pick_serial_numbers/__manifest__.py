8d2377a33387918cf81e045fce1c9c4514c27b6a
ondevelop.sa <ondevelop.sa@gmail.com>
2021-05-03 16:23:27 -0500

    [IMP] Add the help description.

--- pick_serial_numbers/__manifest__.py
+++ pick_serial_numbers/__manifest__.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
+# © 2021 onDevelop.sa
+# Autor: Idelis Gé Ramírez
+# Part of onDevelop.SA. See LICENSE file for full copyright and licensing details.
 {
-    'name': "pick_serial_numbers",
-    'summary': """Serial Number Pick Integration""",
-    'description': """ Permit upload many serial numbers used in the stock
-    picking lines using the product and the pick number.
+    'name': "Pick Serial Numbers Massive Update",
+    'summary': """Massive Upload the Lot/ Serial Number of the stock move line.""",
+    'description': """ Update the Lot/Serial Numbers in the lines of specific
+    stock move using the import default process.
     """,
     'author': "onDevelop.SA",
     'website': "https://ondevelop.tech/",
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Operations/Inventory',
+    'version': '14.0.1',
+    'license': 'LGPL-3',
+    'price': 7,
+    'currency': 'USD',
+    'support': "ondevelop.sa@gmail.com",
     'depends': ['base', 'stock'],
+    'images': ['static/description/pick_serial_number_update_cover.png'],
     'data': [
         # 'security/ir.model.access.csv',
         # 'views/templates.xml',
     ],
-    'demo': ['demo/demo.xml']
+    'demo': ['demo/demo.xml'],
+    'installable': True,
+    'auto_install': False
 }
